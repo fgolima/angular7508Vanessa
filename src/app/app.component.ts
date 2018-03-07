31ad277cc0b7a57ad1302fbb2576f58ffd71458c
fgolima <fgolima@yahoo.com.br>
2018-03-06 22:53:01 -0300

    Final Aula 2 Cap3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core'
+import { HttpClient } from "@angular/common/http"
+
 @Component({
   selector : 'app-root',
 
@@ -30,4 +32,16 @@ import {Component} from '@angular/core'
 })
 export class AppComponent {
 
+  listaFotos
+
+  constructor(conexaoApi: HttpClient) {
+    //console.log('construiu a app');
+
+    conexaoApi.get('http://localhost:3000/v1/fotos')
+              .subscribe( fotosApi => 
+                //console.log(fotosApi)
+                this.listaFotos = fotosApi
+                , erro => console.log(erro)
+              )
+  }
 }   
---
+++ src/app/foto/foto.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector : 'foto',
+    template : `<img src="{{url}}" alt="{{titulo}}" class="responsive-img">`
+})
+export class FotoComponent {
+    @Input() url = ""
+    @Input() titulo = ""
+}
